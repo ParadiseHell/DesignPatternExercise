dcdd79122861f78c76bc9747db3842ade55f38e9
ChengTao <chengtaolearn@163.com>
2016-11-20 18:07:30 +0800

    添加装饰者模式(UDP数据接收)

---
+++ Decorate/src/com/chengtao/impl/UDPReceiverProtocol.java
@@ -0,0 +1,11 @@
+package com.chengtao.impl;
+
+/**
+ * UDP接受接口<br>
+ * @author ChengTao
+ *
+ */
+public interface UDPReceiverProtocol {
+	char getLineChar();
+	char getEndChar();
+}
---
+++ Decorate/src/com/chengtao/receiver/CharReceiver.java
@@ -0,0 +1,37 @@
+package com.chengtao.receiver;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+/**
+ * UDP×Ö·û½ÓÊÜÀà
+ * @author ChengTao
+ *
+ */
+public class CharReceiver {
+	private DatagramSocket socket = null;
+	
+	public CharReceiver(int port) {
+		try {
+			socket = new DatagramSocket(port);
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public char receive() {
+		byte buffer[] = new byte[255];
+		DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+		try {
+			socket.receive(packet);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if (packet.getData() != null) {
+			return (char) packet.getData()[0];
+		}
+		return Character.MIN_VALUE;
+	}
+}
